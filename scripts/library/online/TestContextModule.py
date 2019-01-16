ba67f5365280400f02e6e11b71fd8a64f8a88bfc
x <x@Computer.nubinet>
2019-01-16 12:15:40 -0800

    correct library

--- scripts/library/online/TestContextModule.py
+++
@@ -1,9 +0,0 @@
-from com.muse.fmw.platform import PbProperties
-
-
-class TestContext:
-    def __init__(self, properties):
-        self.props = PbProperties(properties)
-
-    def setKey(self, key):
-        self.key = key
